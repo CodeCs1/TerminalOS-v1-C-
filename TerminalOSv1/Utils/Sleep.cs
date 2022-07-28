07b6ba36e126b2e3f05075531e8e214782446001
CodeCs1 <97139418+CodeCs1@users.noreply.github.com>
2022-07-28 07:53:12 +0700

    Add files via upload

---
+++ TerminalOSv1/Utils/Sleep.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerminalOSv1.Utils
+{
+    public class Sleep
+    {
+        public static void SleepInMilisecond(int ms) // Stops the code for milliseconds and then resumes it (Basically It's delay)
+        {
+            for (int i = 0; i < ms * 100000; i++)
+            {
+                ;
+                ;
+                ;
+                ;
+                ;
+            }
+        }
+    }
+}
